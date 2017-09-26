4b0b5391b7e1262aed6b04471106ee624afb464e
Josh Fenlon <Josh2.Fenlon@live.uwe.ac.uk>
2017-09-26 19:23:11 +0100

    Added 'Climb' function

--- AI Squad controller/Assets/Unit.cs
+++ AI Squad controller/Assets/Unit.cs
@@ -1,10 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Unit : MonoBehaviour {
 
-	float height = 0;
+	public float height = 0;
+	public float maxClimb = 0;
+	public bool awaitingClimb;
+	public GameObject target;
 
+	void Update() {
+		if (GetComponent<NavMeshAgent> ().remainingDistance < 1 && awaitingClimb) {
+			if (target.GetComponent<navigatableObject> ().clickedOn (this)) {
+				climbOver ();
+			} else {
+				GetComponent<NavMeshAgent> ().ResetPath ();
+			}
+			awaitingClimb = false;
+		}
+	}
+
+	public void climbOver()
+	{
+		Vector3 mirrorPos = Vector3.zero;
+
+		if ((target.transform.position.x - target.transform.localScale.x) < transform.position.x
+			&& transform.position.x < (target.transform.position.x + target.transform.localScale.x)) {
+			transform.position += new Vector3 (0, 0, (target.transform.position.z - transform.position.z) * 2);
+		}
+
+		if ((target.transform.position.z - target.transform.localScale.z) < transform.position.z
+			&& transform.position.z < (target.transform.position.z + target.transform.localScale.z)) {
+			transform.position += new Vector3 ((target.transform.position.x - transform.position.x) * 2, 0, 0);
+		}
+
+		GetComponent<NavMeshAgent> ().ResetPath ();
+		GetComponent<NavMeshAgent> ().velocity = Vector3.zero;
+	}
 
 }
--- AI Squad controller/Assets/navigatableObject.cs
+++ AI Squad controller/Assets/navigatableObject.cs
@@ -10,17 +10,19 @@ public class navigatableObject : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	public bool clickedOn (float height) {
-		if (gameObject.isStatic) {
-			canClimb (height);
+	public bool clickedOn (Unit other) {
+		if (canClimb (other)) {
 			return true;
 		}
 		return false;
 	}
 
-	bool canClimb (float height) {
-		RaycastHit hit;
-		Physics.Raycast (transform.position + (Vector3.up * height), Vector3.down, out hit, height);
-		return hit.collider.gameObject == gameObject;
+	bool canClimb (Unit other) {
+		if ((transform.position.y + transform.localScale.y) - (other.transform.position.y - other.transform.localScale.y) < other.maxClimb) {
+			RaycastHit hit;
+			Physics.Raycast (transform.position + (Vector3.up * other.height - new Vector3 (0, -0.1f, 0)), Vector3.down, out hit, other.height);
+			return hit.collider.gameObject == gameObject;
+		}
+		return false;
 	}
 }
