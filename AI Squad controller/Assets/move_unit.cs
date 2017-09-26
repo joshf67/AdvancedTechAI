7316998b781f7a6a3de649512364fceab036bf91
josh <iownall99@live.co.uk>
2017-09-26 17:41:47 +0100

    Added base unity project

---
+++ AI Squad controller/Assets/move_unit.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class move_unit : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			RaycastHit hit;
+			Ray mousePoint = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (mousePoint,out hit)) {
+				GameObject[] objects = GameObject.FindGameObjectsWithTag ("Unit");
+				foreach (GameObject obj in objects) {
+					obj.GetComponent<NavMeshAgent> ().SetDestination (hit.point);
+				}
+				if (hit.collider.gameObject.GetComponent<navigatableObject>()) {
+					if (hit.collider.gameObject.GetComponent<navigatableObject> ().clickedOn(1)) {
+						bool repeat = true;
+						List<GameObject> checking = new List<GameObject> ();
+						foreach (GameObject obj in objects) {
+							checking.Add (obj);
+						}
+						while (repeat) {
+							foreach (GameObject obj in checking) {
+								
+								if (Vector3.Distance (obj.transform.position, (new Vector3 (hit.point.x, 0, hit.point.z))) < 1) {
+									obj.transform.position += Vector3.Angle (obj.transform.position, hit.point) * (new Vector3 (hit.collider.transform.localScale.x, 0, hit.collider.transform.localScale.z));
+									checking.Remove (obj);
+								}
+							}
+							if (checking.Count == 0) {
+								repeat = false;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ AI Squad controller/Assets/navigatableObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class navigatableObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	public bool clickedOn (float height) {
+		if (gameObject.isStatic) {
+			canClimb (height);
+			return true;
+		}
+		return false;
+	}
+
+	bool canClimb (float height) {
+		RaycastHit hit;
+		Physics.Raycast (transform.position + (Vector3.up * height), Vector3.down, out hit, height);
+		return hit.collider.gameObject == gameObject;
+	}
+}
